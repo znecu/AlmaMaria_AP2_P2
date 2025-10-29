5a57ba993054de7191aab7c838efc9e1df47d0fd
Alma Maria <alma_maria@ucne.edu.do>
2025-10-29 19:17:57 -0400

    configurando el hilt

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/AndroidApp.kt
@@ -0,0 +1,7 @@
+package edu.ucne.almamaria_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AndroidApp: Application(){}
--- app/src/main/java/edu/ucne/almamaria_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 import edu.ucne.almamaria_ap2_p2.ui.theme.AlmaMaria_AP2_P2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
