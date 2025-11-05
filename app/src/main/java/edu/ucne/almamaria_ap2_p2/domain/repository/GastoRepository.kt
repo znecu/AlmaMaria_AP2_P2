8fc22a4f9f70b780c8fa7be43e1ceae83c77fa99
Alma Maria <alma_maria@ucne.edu.do>
2025-11-05 18:45:27 -0400

    creé GastoRepository.kt

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/domain/repository/GastoRepository.kt
@@ -0,0 +1,13 @@
+package edu.ucne.almamaria_ap2_p2.domain.repository
+
+import edu.ucne.almamaria_ap2_p2.data.remote.Resource
+import edu.ucne.almamaria_ap2_p2.domain.model.Gasto
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface GastoRepository {
+    fun getGasto(id: Int): Flow<Resource<List<Gasto>>>
+
+    suspend fun saveGasto(gasto: Gasto): Resource<Unit>
+
+}
