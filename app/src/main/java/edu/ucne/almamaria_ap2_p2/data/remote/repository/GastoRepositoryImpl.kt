64b08c2d601bd0460214f86fa5e775bd596f1612
Alma Maria <alma_maria@ucne.edu.do>
2025-11-05 19:25:36 -0400

    creé la implementacion del repositorio

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/data/remote/repository/GastoRepositoryImpl.kt
@@ -0,0 +1,42 @@
+package edu.ucne.almamaria_ap2_p2.data.remote.repository
+
+import androidx.compose.ui.geometry.Rect
+import coil.network.HttpException
+import edu.ucne.almamaria_ap2_p2.data.mapper.toDomain
+import edu.ucne.almamaria_ap2_p2.data.mapper.toDto
+import edu.ucne.almamaria_ap2_p2.data.remote.RemoteDataSource
+import edu.ucne.almamaria_ap2_p2.data.remote.Resource
+import edu.ucne.almamaria_ap2_p2.domain.model.Gasto
+import edu.ucne.almamaria_ap2_p2.domain.repository.GastoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+data class GastoRepositoryImpl @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) : GastoRepository {
+    override fun getGasto(id: Int): Flow<Resource<List<Gasto>>> = flow {
+        try {
+            emit(Resource.Loading<List<Gasto>>())
+            val gastoDto = remoteDataSource.getGasto(id)
+            val gasto = gastoDto.map { it.toDomain() }
+            emit(Resource.Success(gasto))
+        } catch (e: HttpException) {
+            emit(Resource.Error("Error de servidor: ${e.message}"))
+        } catch (e: Exception) {
+            emit(Resource.Error("Error desconocido: ${e.localizedMessage}"))
+        }
+    }
+
+    override suspend fun saveGasto(gasto: Gasto): Resource<Unit> {
+        return try {
+            val dto = gasto.toDto()
+            remoteDataSource.saveGasto(dto)
+            Resource.Success(Unit)
+        } catch (e: HttpException){
+            Resource.Error("Error de seervidor: ${e.message}")
+        } catch (e: Exception){
+            Resource.Error("Error desconocido: ${e.localizedMessage}")
+        }
+    }
+}
