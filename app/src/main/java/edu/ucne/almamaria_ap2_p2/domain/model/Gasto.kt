a940438ed9c2c2d677c16e84434d295ead503387
Alma Maria <alma_maria@ucne.edu.do>
2025-11-05 18:43:33 -0400

    creé el modelo de gasto

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/domain/model/Gasto.kt
@@ -0,0 +1,10 @@
+package edu.ucne.almamaria_ap2_p2.domain.model
+
+data class Gasto(
+    val gastoId: Int,
+    val fecha: String,
+    val suplidor: String,
+    val ncf: String,
+    val itbis: Double,
+    val monto: Double
+)
