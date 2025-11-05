edba5bf66b0d3e55c73b4fe00bf67b24609a34c8
Alma Maria <alma_maria@ucne.edu.do>
2025-11-05 19:39:52 -0400

    coloqué la url de la api e implementé el modulo del repositorio

--- app/src/main/java/edu/ucne/almamaria_ap2_p2/di/ApiModule.kt
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/di/ApiModule.kt
@@ -2,22 +2,24 @@ package edu.ucne.almamaria_ap2_p2.di
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import edu.ucne.almamaria_ap2_p2.data.remote.GastoApi
+import edu.ucne.almamaria_ap2_p2.data.remote.repository.GastoRepositoryImpl
+import edu.ucne.almamaria_ap2_p2.domain.repository.GastoRepository
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.create
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object ApiModule {
-    private const val BASE_URL = "" //TODO: colocar la URL
+    private const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net"
 
     @Provides
     @Singleton
@@ -46,5 +48,15 @@ object ApiModule {
             .client(okHttpClient)
             .build()
             .create(GastoApi::class.java)
+    }
+
+    @InstallIn(SingletonComponent::class)
+    @Module
+    abstract class RepositoryModule{
+        @Binds
+        @Singleton
+        abstract fun bindGastoRepository(
+            gastoRepositoryImpl: GastoRepositoryImpl
+        ): GastoRepository
     }
 }
