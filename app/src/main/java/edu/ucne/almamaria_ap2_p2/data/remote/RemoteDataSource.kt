12e576b1c2ed5ff6d14eaa6ce49c9865257110cc
Alma Maria <alma_maria@ucne.edu.do>
2025-11-05 18:45:35 -0400

    creé RemoteDataSource.kt

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/data/remote/RemoteDataSource.kt
@@ -0,0 +1,13 @@
+package edu.ucne.almamaria_ap2_p2.data.remote
+
+import edu.ucne.almamaria_ap2_p2.data.remote.GastoApi
+import edu.ucne.almamaria_ap2_p2.data.remote.dto.GastoDto
+import javax.inject.Inject
+
+data class RemoteDataSource @Inject constructor(
+    private val gastoApi: GastoApi
+){
+    suspend fun getGasto(id: Int): List<GastoDto> = gastoApi.getGasto(id)
+
+    suspend fun saveGasto(gastoDto: GastoDto) = gastoApi.saveGasto(gastoDto)
+}
