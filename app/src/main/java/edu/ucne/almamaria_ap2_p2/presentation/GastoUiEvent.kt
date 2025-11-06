c18fd1b2914bee2a59431fda3cfa703bb70ff09b
Alma Maria <alma_maria@ucne.edu.do>
2025-11-05 20:39:27 -0400

    creé el GastoUiEvent.kt

---
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/presentation/GastoUiEvent.kt
@@ -0,0 +1,17 @@
+package edu.ucne.almamaria_ap2_p2.presentation
+
+interface GastoUiEvent {
+    data object Load : GastoUiEvent
+    data class CreateNew(val fecha: String, val suplidor: String, val ncf: String,
+                         val itbis: Double, val monto: Double): GastoUiEvent
+
+    object ShowBottonSheet : GastoUiEvent
+    object HideBottonSheet : GastoUiEvent
+
+    data class OnFechaChange(val value: String): GastoUiEvent
+    data class OnSuplidorChange(val value: String): GastoUiEvent
+    data class OnNcfChange(val value: String): GastoUiEvent
+    data class OnItbisChange(val value: String): GastoUiEvent
+    data class OnMontoChange(val value: String): GastoUiEvent
+
+}
