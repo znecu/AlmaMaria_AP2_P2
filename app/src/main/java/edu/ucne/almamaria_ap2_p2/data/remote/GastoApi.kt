276752229178a31da95b04b76a69a0b63c93b4c7
Alma Maria <alma_maria@ucne.edu.do>
2025-11-06 00:10:20 -0400

    agregando el endpoint faltante y corrección de otro

--- app/src/main/java/edu/ucne/almamaria_ap2_p2/data/remote/GastoApi.kt
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/data/remote/GastoApi.kt
@@ -4,13 +4,17 @@ import edu.ucne.almamaria_ap2_p2.data.remote.dto.GastoDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface GastoApi {
 
-    @GET("api/Gasto/{gastoId}")
+    @GET("api/Gastos")
+    suspend fun getGastos(): List<GastoDto>
+    @GET("api/Gastos/{gastoId}")
     suspend fun getGasto(@Path("gastoId") id: Int): List<GastoDto>
 
-    @POST("api/Gasto")
+    @POST("api/Gastos")
     suspend fun saveGasto(@Body gastoDto: GastoDto)
+
 }
