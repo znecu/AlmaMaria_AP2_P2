5956a50bc2bc294388d028fa9e2d8ad2e4319d49
Alma Maria <alma_maria@ucne.edu.do>
2025-11-06 00:11:53 -0400

    agregando datos faltantes

--- app/src/main/java/edu/ucne/almamaria_ap2_p2/presentation/GastoUiState.kt
+++ app/src/main/java/edu/ucne/almamaria_ap2_p2/presentation/GastoUiState.kt
@@ -1,10 +1,18 @@
 package edu.ucne.almamaria_ap2_p2.presentation
 
+import edu.ucne.almamaria_ap2_p2.data.remote.Resource
 import edu.ucne.almamaria_ap2_p2.domain.model.Gasto
 
 data class GastoUiState(
-    val gasto: List<Gasto> = emptyList(),
+    val ListaGastos: Resource<List<Gasto>> = Resource.Loading(),
     val isLoading: Boolean = false,
     val message: String? = null,
-    val error: String? = null
+    val error: String? = null,
+
+    val gastoId: Int = 0,
+    val gastoFecha: String = "",
+    val gastoSuplidor: String = "",
+    val gastoNcf: String = "",
+    val gastoItbis: String = "",
+    val gastoMonto: String = "",
 )
